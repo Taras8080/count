b5ce442369e43af168b9423ca8b2563f93af45e1
Taras8080 <tarasfeduna@gmail.com>
2023-01-27 19:11:04 +0200

    front-end

--- skript.js
+++ skript.js
@@ -1,1 +1,26 @@
-const count =  document.querySelector()
+let counter = 0;
+const counterEl = document.querySelector('.textt')
+const startBt = document.querySelector('#startbtn')
+const pauseBt = document.querySelector('#pausebtn')
+const stopBt = document.querySelector('#stopbtn')
+let lit;
+
+
+startBt.onclick = function() {
+    lit = setInterval(function(){
+        counter++;
+        counterEl.innerText = counter;
+        console.log(counter)
+    }, 1000)
+}
+
+pauseBt.onclick = function () {
+clearInterval(lit)
+}
+
+
+stopBt.onclick = function() {
+    counter = 0;
+    counterEl.innerText = 'Я люблю Софійку і Лілю'
+    clearInterval(lit)
+}
